10df616e792419dad597a19412be12e2563207a3
Mihail Bodrov <connormiha1@gmail.com>
2016-12-08 02:06:43 +0300

    migrate to typescript 2.1

--- src/additionTypings.d.ts
+++ src/additionTypings.d.ts
@@ -2,11 +2,6 @@ declare var immutable: any;
 declare var global: any;
 declare var require: any;
 
-declare module 'bem-cn' {
-    var x: any;
-    export = x;
-}
-
 declare module 'redux-devtools-extension/developmentOnly' {
     export function composeWithDevTools(arg: any): any;
 }
