0c6df015b62dd73031631befa6eadd525991c636
Mihail Bodrov <connormiha1@gmail.com>
2017-01-01 03:19:50 +0300

    add new game_7

---
+++ src/games/game7/characters.ts
@@ -0,0 +1,27 @@
+const characters = {
+    farmer: {
+        name: 'Farmer'
+    },
+
+    cabbage: {
+        name: 'Cabbage'
+    },
+
+    sheep: {
+        name: 'Sheep'
+    },
+
+    wolf1: {
+        name: 'Wolf'
+    },
+
+    wolf2: {
+        name: 'Wolf'
+    },
+
+    dog: {
+        name: 'Dog'
+    }
+};
+
+export default characters;
---
+++ src/games/game7/index.ts
@@ -0,0 +1,95 @@
+import characters from './characters';
+
+import {
+    RIVERSIDE_LEFT, RIVERSIDE_RIGHT, BOAT,
+    Game, landingValidator, depetureValidator
+} from 'games/helpers';
+
+const game: Game = {
+    title: 'Crossing 7',
+    description: 'It is necessary to get everyone across the river.',
+    rules: {
+        beforeLanding: [
+            {
+                description: 'The boat can accommodate only 3 persons',
+                check(collocation: any, characterId: string, moveTo: string): boolean {
+                    return moveTo !== BOAT || collocation.boat.length < 3;
+                }
+            }
+        ],
+        beforeDeparture: [
+            {
+                description: 'Driving the boat can only farmer',
+                check(collocation: any): boolean {
+                    return collocation.boat.includes('farmer');
+                }
+            },
+            {
+                description: 'Do not leave a wolf and a sheep alone without the farmer',
+                check(collocation: any): boolean {
+                    for (let item of [RIVERSIDE_LEFT, RIVERSIDE_RIGHT]) {
+                        let side = collocation[item];
+
+                        if (!side.includes('farmer') && (side.includes('wolf1') || side.includes('wolf2')) && side.includes('sheep')) {
+                            return false;
+                        }
+                    }
+
+                    return true;
+                }
+            },
+            {
+                description: 'Do not leave a wolf and a dog alone without the farmer',
+                check(collocation: any): boolean {
+                    for (let item of [RIVERSIDE_LEFT, RIVERSIDE_RIGHT]) {
+                        let side = collocation[item];
+
+                        if (!side.includes('farmer') && (side.includes('wolf1') || side.includes('wolf2')) && side.includes('dog')) {
+                            return false;
+                        }
+                    }
+
+                    return true;
+                }
+            },
+            {
+                description: 'Do not leave a dog and a sheep alone without the farmer',
+                check(collocation: any): boolean {
+                    for (let item of [RIVERSIDE_LEFT, RIVERSIDE_RIGHT]) {
+                        let side = collocation[item];
+
+                        if (!side.includes('farmer') && side.includes('dog') && side.includes('sheep')) {
+                            return false;
+                        }
+                    }
+
+                    return true;
+                }
+            },
+            {
+                description: 'Do not leave the sheep and cabbage alone without the farmer',
+                check(collocation: any): boolean {
+                    for (let item of [RIVERSIDE_LEFT, RIVERSIDE_RIGHT]) {
+                        let side = collocation[item];
+
+                        if (!side.includes('farmer') && side.includes('cabbage') && side.includes('sheep')) {
+                            return false;
+                        }
+                    }
+
+                    return true;
+                }
+            }
+        ]
+    },
+    landingValidator,
+    depetureValidator,
+    characters,
+    collocation: {
+        [BOAT]: [],
+        [RIVERSIDE_LEFT]: Object.keys(characters),
+        [RIVERSIDE_RIGHT]: []
+    }
+};
+
+export default game;
