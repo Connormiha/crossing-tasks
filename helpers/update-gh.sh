44958c0f0ad86b2ae831c7761029724cb4965447
Mihail Bodrov <connormiha1@gmail.com>
2017-10-04 03:04:37 +0300

    Minor updates :art:

--- helpers/update-gh.sh
+++ helpers/update-gh.sh
@@ -3,9 +3,9 @@
 npm run build:gh
 git checkout gh-names
 rm -fr static index.html 404.html
-name="${PWD##*/}"
-cp build/"${name}"/index.html ./
+cp build/index.html ./
 cp index.html 404.html
+name="${PWD##*/}"
 cp -R build/"${name}"/* ./
 git add .
 git commit -m "Update build :arrow_up:"
